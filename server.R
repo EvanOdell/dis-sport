7864c3ad22477c7d36d79047b5ddde87c9ee8160
EvanOdell <evanodell91@gmail.com>
2016-12-20 13:08:03 +0000

    icon fix?

--- server.R
+++ server.R
@@ -9,12 +9,12 @@ shinyServer(function(input, output, session) {
   
   GYA <- readRDS("GYA.rds")
   
-  GYA_Icon <- makeIcon("GYA.PNG",25,25)
+  GYA_Icon <- makeIcon("./GYA.PNG","./GYA.PNG",25,25)
   
   typeIcons <- iconList(
-    Disability = makeIcon("./disability.PNG", "disability.PNG",30,30),
-    "Disability and Sport" = makeIcon("./disSport.PNG", "disSport.PNG",30,30),
-    Sport = makeIcon("./sport.png", "sport.png",30,30)
+    Disability = makeIcon("./disability.PNG", "./disability.PNG",30,30),
+    "Disability and Sport" = makeIcon("./disSport.PNG", "./disSport.PNG",30,30),
+    Sport = makeIcon("./sport.png", "./sport.png",30,30)
   )
   
   output$mymap <- renderLeaflet({
