82ef4ab4da07f4249b23b2f604b0876371f89837
EvanOdell <evanodell91@gmail.com>
2017-02-23 12:14:15 +0000

    commit

--- server.R
+++ server.R
@@ -88,11 +88,12 @@ shinyServer(function(input, output, session) {
       server = TRUE, 
       autoWidth = FALSE,
       columnDefs = list(list(visible=FALSE, targets=list(0,5,6,7,8,9,10,
-                                                        11,12,13,14,15,16))),
+                                                        11,12,13,14,15,16,
+                                                        17))),
       dom = 'Blfrtip',
       buttons = c(list(list(extend = 'colvis', columns = c(1,2,3,4,5,6,7,8,
                                                          9,10,11,12,13,14,
-                                                         15,16),visible=FALSE))
+                                                         15,16,17),visible=FALSE))
     ))))
   
   observe({
