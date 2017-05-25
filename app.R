2a8593ee6044f27623750214c0df546e49950779
EvanOdell <evanodell91@gmail.com>
2017-05-25 10:02:51 +0100

    map-making tutorial

---
+++ app.R
@@ -0,0 +1,224 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(DT)
+library(readr)
+
+
+# Define UI for application that draws a histogram
+header <- dashboardHeader(title='Disability Sport Charities',
+                             titleWidth = 350)
+
+shinyjs::useShinyjs()
+tags$head(
+  tags$link(rel = "stylesheet", type = "text/css", href = "custom.css")
+)
+
+body <- dashboardBody(
+  #h4(a(icon("futbol-o"),"Return to getyourselfactive.org", href="http://getyourselfactive.org")),
+  #h5(a(icon("home"),"Return to evanodell.com", href="http://shiny.evanodell.com/")),
+  fluidRow(column(width = 8,offset = 1,
+                  box(
+                    width = NULL, solidHeader = TRUE,
+                    leafletOutput("mymap")
+                  ),
+                  box(width=NULL,
+                      dataTableOutput("ds_dt")
+                  ),
+               includeMarkdown("./assets/summary.Rmd")),
+        column(width=2,
+               box(width=NULL, 
+                   checkboxGroupInput("category_input",
+                                      "Category",c("Disability and Sport"='Disability and Sport',
+                                                   "Disability"="Disability",
+                                                   "Sport"="Sport"),
+                                      selected = "Disability and Sport")),
+               box(width=NULL, h5("Options"),
+                   checkboxInput("show_penguins",
+                                 "Show Get Yourself Active Partners",
+                                 value = FALSE),
+                   checkboxInput("map_style",
+                                 "Use High Contrast Map",
+                                 value = FALSE)),
+               box(width=NULL, 
+                   downloadButton('download_data',
+                                  'Download Table')),
+               
+               
+               box(width=NULL,
+                   actionButton("reset", "Reset Search"))
+        )
+    )
+  )
+
+# Run the application 
+shinyApp(
+  ui = dashboardPage(
+    header,
+    dashboardSidebar(disable = TRUE),
+    skin = "green",
+    body
+  ),
+
+server <- function(input, output, session) {
+    
+    dis_sport<- read_rds("./data/dis_sport.rds")
+    
+    GYA <- read_rds("./data/GYA.rds")
+    
+    GYA_Icon <- makeIcon("./img/GYA.png","./img/GYA.png",25,25)
+    
+    typeIcons <- iconList(
+      Disability = makeIcon("./img/disability.png", "./img/disability.png",30,30),
+      "Disability and Sport" = makeIcon("./img/disSport.png", "./img/disSport.png",30,30),
+      Sport = makeIcon("./img/sport.png", "./img/sport.png",30,30)
+    )
+    
+    observeEvent(input$reset,{
+      
+      shinyjs::reset("myapp")
+      leafletProxy("mymap") %>% 
+        setView(lng = -2.547855, lat = 54.00366, zoom = 5)
+      
+    })
+    
+    
+    output$mymap <- renderLeaflet({
+      
+      leaflet() %>%
+        setView(lng = -2.547855, lat = 54.00366, zoom = 5)
+      
+    })
+    
+    getDataSet <- reactive({
+      
+      if (is.null(input$mymap_bounds))
+        return(dis_sport[FALSE,])
+      
+      bounds <- input$mymap_bounds
+      latRng <- range(bounds$north, bounds$south)
+      lngRng <- range(bounds$east, bounds$west)
+      
+      subset(dis_sport, category %in% input$category_input & 
+               latitude >= latRng[1] & latitude <= latRng[2] &
+               longitude >= lngRng[1] & longitude <= lngRng[2])
+      
+    })
+    
+    output$ds_dt = renderDataTable(datatable({
+      dataSet <- getDataSet()
+      
+    },
+    filter = 'top',
+    colnames = c('Name' = 'name',
+                 'Charitable Object' = 'object',
+                 'District' = 'district',
+                 'Region' = 'region',
+                 'Address' = 'address',
+                 'Phone' = 'phone',
+                 'Website' = 'web',
+                 'Category' = 'category',
+                 'Latitude'= 'latitude',
+                 'Longitude'= 'longitude'),
+    
+    extensions = c('FixedHeader','Buttons'),
+    escape = FALSE,
+    options = list(
+      lengthMenu = list(c(5, 10, -1), c('5', '10', 'All')),
+      order = list(2, 'asc'),
+      pageLength = 5, 
+      fixedHeader = TRUE,
+      server = TRUE, 
+      autoWidth = FALSE,
+      columnDefs = list(list(visible=FALSE, targets=list(0,2,3,4,5,6,7))),
+      dom = 'Blfrtip',
+      buttons = c(list(list(extend = 'colvis', columns = c(1,2,3,4,5,6,7,8,9,10),visible=FALSE))
+      ))))
+    
+    
+    observe({
+      
+      dataSet<-getDataSet() 
+      
+      filteredData <- reactive({
+        
+        s2 <- input$ds_dt_rows_all
+        
+        dataSet[s2, , drop = FALSE]
+        
+      })
+      
+      if(input$map_style==TRUE){
+        provider <- providers$Stamen.Toner
+      } else {
+        provider <- providers$Esri.WorldStreetMap
+      }
+      
+      if(nrow(filteredData())==0) { leafletProxy("mymap") %>% clearShapes()} 
+      
+      else {
+        
+        leafletProxy("mymap", data = filteredData()) %>%
+          clearGroup(group="charities") %>% addProviderTiles(provider) %>%
+          addMarkers(~longitude, 
+                     ~latitude,
+                     icon = ~typeIcons[category],
+                     popup=~as.character(paste("<strong>Name:</strong> ", name, "<br>",
+                                               "<strong>Area of Focus:</strong> ", category, "<br>",
+                                               "<br>",
+                                               "<strong>Contact</strong>", "<br>",
+                                               "<em>Address:</em> ", address,"<br>",
+                                               "<em>Phone:</em> ", phone,"<br>",
+                                               "<em>Website:</em> ", web)),
+                     group="charities",
+                     clusterOptions = markerClusterOptions())
+        
+        if (input$show_penguins==TRUE) {
+          leafletProxy("mymap", data = GYA) %>%
+            addMarkers(~longitude, 
+                       ~latitude, 
+                       icon=GYA_Icon,
+                       popup=~as.character(paste("<strong>Get Yourself Active Partner</strong>", "<br>",
+                                                 "Name: ", GYA$name, "<br>",
+                                                 "<br>",
+                                                 "<strong>Contact</strong>", "<br>",
+                                                 "<em>Address:</em> ", address,"<br>",
+                                                 "<em>Phone:</em> ", phone,"<br>",
+                                                 "<em>Website:</em> ", web)),
+                       group="GYA")
+        } else {
+          
+          leafletProxy("mymap") %>% clearGroup(group="GYA")
+          
+        }
+        
+      }
+      
+      output$download_data = downloadHandler(paste0('disability_sport_', Sys.Date(),".csv"), content = function(file) {
+        
+        s2 <- input$ds_dt_rows_all
+        
+        dataSet$web <- gsub("<a href= '", "", dataSet$web)
+        
+        dataSet$web <- gsub("'>.*", "", dataSet$web)
+        
+        #dataSet$web <- gsub("</a>*", "", dataSet$web)
+        
+        write_csv(dataSet[s2, , drop = FALSE], file)
+        
+      })
+      
+      
+    })
+    
+  })
+  
---
+++ pyscripts/README.md
@@ -0,0 +1,51 @@
+# charity-commission-extract
+Python utilities for handling the import of data from the Charity Commission data extract
+
+## Get the data
+
+The data can be accessed from <http://data.charitycommission.gov.uk/default.aspx>
+
+## Beginner's Guide
+
+A [beginner's guide](beginners-guide.md) to using the Charity Commission data extract.
+
+Marc Lawson at NCVO has also written [a guide to getting the data into a database](https://data.ncvo.org.uk/a/almanac16/how-to-create-a-database-for-charity-commission-data/).
+
+## Utilities
+
+### import.py
+
+This script accepts a ZIP file containing the Charity Commission data extract and converts 
+it into CSV files. It uses the bcp.py script to convert the .bcp files into .csv. It also 
+adds column headings to the beginning of the files.
+
+#### Usage
+
+1. Download the Charity register extract ZIP file using the link above.
+2. Open the command line and navigate to the folder where you want to store the files
+3. Run the command `python /path/to/script/import.py /path/to/download/zip/RegPlusExtract_February_2015.zip`
+
+The CSV files will be created in the directory you are in.
+
+To use elsewhere use the function `import_zip(zipfile)` where zipfile is the path to the zip.
+
+### bcp.py
+
+This script converts a .bcp file (which uses the delimiters `@**@` and `*@@*`) into a more
+standard CSV file. It can be configured to use any delimiters accepted by python's CSV
+utility.
+
+#### Usage
+
+Use the function `convert(bcpdata, csvfilename="", lineterminator='*@@*', delimiter='@**@', quote='"', newdelimiter=';')`.
+
+- `bcpdata` the full contents of the BCP file
+- `csvfilename` the path to the output file (will be "converted.csv" if left blank
+- `lineterminator` the line terminator used in the BCP file (default is `*@@*`)
+- `delimiter` the column delimiter used in the BCP file (default is `@**@`)
+- `quote` the character used to quote strings (default is `"`)
+- `newdelimiter` character used to delimit the CSV file (default is `,`)
+
+Note that the function puts every field in the CSV file in quotes, regardless of the type of field
+(so numbers, dates, etc are all quoted). This will need to be taken into account when importing data
+from these CSV files.
